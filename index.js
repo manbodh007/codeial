ed7263ba3425b6f7235f3d899cac2cccdbb82b49
manbodh <manbodhratre1@gmail.com>
2020-05-23 22:57:18 +0530

    set view

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 app.use('/',require('./routers/index'));
+//set view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(error){
     if(error){
